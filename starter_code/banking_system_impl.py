0c038b50a0262c3d35bf29cebee959ff9300233f
Esther Mathew <esther_mathew@berkeley.edu>
2024-11-23 14:40:01 -0800

    updating with level 4 code from 11_23

--- starter_code/banking_system_impl.py
+++ starter_code/banking_system_impl.py
@@ -5,17 +5,17 @@ import math
 from banking_system import BankingSystem
 
 class Account:
-    def __init__(self, id, balance=0, total_outgoing=0): 
+    def __init__(self, timestamp, id, balance=0, total_outgoing=0): 
+        self.creation_timestamp = timestamp
         self.id = id
         self.balance = balance
+        self.balance_history = {timestamp: balance}
         self.total_outgoing = total_outgoing
-        #self.payments = {}
-        #self.transaction_history = {}
-        #self.timestamp = datetime.timestamp()
         
     # add amount if transferred to account
     def deposit(self, timestamp: int, amount: int):
         self.balance += amount
+        self.balance_history[timestamp] = self.balance
         """
         if timestamp not in self.transaction_history.keys():
             self.transaction_history[timestamp] = [amount]
@@ -27,6 +27,7 @@ class Account:
     # decrease amount if withdrawn from account
     def withdraw(self, timestamp: int, amount: int): 
         self.balance -= amount
+        self.balance_history[timestamp] = self.balance
         self.total_outgoing += amount
         """
         if timestamp not in self.transaction_history.keys():
@@ -52,7 +53,8 @@ class BankingSystemImpl(BankingSystem):
             return False
         # creates account with unique id and adds account to accounts dictionary
         else:
-            self.accounts[account_id] = Account(account_id)
+            self.accounts[account_id] = Account(timestamp, account_id)
+            
             return True
 
     def deposit(self, timestamp: int, account_id: str, amount: int) -> int | None:
@@ -140,6 +142,7 @@ class BankingSystemImpl(BankingSystem):
             self.accounts[cashback_account_id].deposit(cashback_time, cashback_amount)   
             self.completed_cashbacks.append([cashback_time, cashback_account_id, payment_id, cashback_amount])
         
+        
     def get_payment_status(self, timestamp: int, account_id: str, payment: str) -> str | None:
         """
         Should return the status of the payment transaction for the
@@ -173,7 +176,106 @@ class BankingSystemImpl(BankingSystem):
                 return "CASHBACK_RECEIVED"
         
         return None
-        
+    
+    def merge_cashbacks(self, timestamp: int, account_id_1: str, account_id_2: str):
+        for i, cashback in enumerate(self.pending_cashbacks): 
+            cashback_time, cashback_account_id, cashback_payment_id, cashback_amount = cashback 
+            if cashback_account_id == account_id_2:
+                updated_cashback = (cashback_time, account_id_1, cashback_payment_id, cashback_amount)
+                self.pending_cashbacks[i] = updated_cashback
+                heapq.heapify(self.pending_cashbacks)
 
+        for i, cashback in enumerate(self.completed_cashbacks): 
+            cashback_time, cashback_account_id, cashback_payment_id, cashback_amount = cashback 
+            if cashback_account_id == account_id_2:
+                updated_cashback = (cashback_time, account_id_1, cashback_payment_id, cashback_amount)
+                self.completed_cashbacks[i] = updated_cashback
+                heapq.heapify(self.completed_cashbacks)            
         
+    
+    def merge_accounts(self, timestamp: int, account_id_1: str, account_id_2: str) -> bool:
+        """
+        Should merge `account_id_2` into the `account_id_1`.
+        Returns `True` if accounts were successfully merged, or
+        `False` otherwise.
+        Specifically:
+          * Returns `False` if `account_id_1` is equal to
+          `account_id_2`.
+          * Returns `False` if `account_id_1` or `account_id_2`
+          doesn't exist.
+          * All pending cashback refunds for `account_id_2` should
+          still be processed, but refunded to `account_id_1` instead.
+          * After the merge, it must be possible to check the status
+          of payment transactions for `account_id_2` with payment
+          identifiers by replacing `account_id_2` with `account_id_1`.
+          * The balance of `account_id_2` should be added to the
+          balance for `account_id_1`.
+          * `top_spenders` operations should recognize merged accounts
+          - the total outgoing transactions for merged accounts should
+          be the sum of all money transferred and/or withdrawn in both
+          accounts.
+          * `account_id_2` should be removed from the system after the
+          merge.
+        """
+        if account_id_1 == account_id_2:
+            return False
+        if (account_id_1 not in self.accounts.keys()) or (account_id_2 not in self.accounts.keys()):
+            return False
+        # update balance of account 1 to include account 2 balance
+        self.accounts[account_id_1].deposit(timestamp, self.accounts[account_id_2].balance)
         
+        # update total outgoing of account 1 to include account 2 total outgoing
+        self.accounts[account_id_1].total_outgoing += self.accounts[account_id_2].total_outgoing
+        # update the balance history of account 1 to include account 2 balance history
+        #self.accounts[account_id_1].balance_history.update(self.accounts[account_id_2].balance_history)
+        
+        #for time, balance in self.accounts[account_id_1].balance_history.items(): 
+            #if time <= timestamp
+                #self.accounts[account_id_2].balance_history[time] = balance
+                
+        # call to different function for updating pending_cashbacks and completed_cashbacks with account id 1 for account id 2
+        self.merge_cashbacks(timestamp, account_id_1, account_id_2)
+        # take account 2 id out of the accounts dictionary
+        self.accounts.pop(account_id_2)
+        
+        return True
+
+    def get_balance(self, timestamp: int, account_id: str, time_at: int) -> int | None: 
+        '''
+        should return the total amount of money in the account account_id at the given timestamp time_at. If the specified 
+        account did not exist at a given time time_at, returns None. 
+        o If queries have been processed at timestamp time_at, get_balance must reflect the 
+            account balance after the query has been processed. 
+        o If the account was merged into another account, the merged account should inherit its 
+            balance history.'''
+        if account_id not in self.accounts.keys():
+            if isinstance(account_id, Account):
+                if account_id.creation_timestamp > time_at:
+                    return None
+            else: 
+                greatest_time_at_key = None                
+                for key in Account[account_id].balance_history.keys():
+                    if key <= time_at:
+                        if greatest_time_at_key is None or greatest_time_at_key <= key:
+                            greatest_time_at_key = key
+                print(greatest_time_at_key)
+                
+                return Account[account_id].balance_history[greatest_time_at_key]
+            
+        #if account DNE at time_at, return none
+        if self.accounts[account_id].creation_timestamp > time_at:
+            return None   
+        else:
+            #if pending queries exist at same time_at, process queries then get_balance
+            self.process_cashbacks(time_at)
+            #else return total_amount at time_at
+            greatest_time_at_key = None                
+            for key in self.accounts[account_id].balance_history.keys():
+                if key <= time_at:
+                    if greatest_time_at_key is None or greatest_time_at_key <= key:
+                        greatest_time_at_key = key
+            if greatest_time_at_key is None: 
+                raise ValueError(f"nothing in {account_id} at {time_at}")
+            print(greatest_time_at_key)
+            
+            return self.accounts[account_id].balance_history[greatest_time_at_key]
